8a69e2530164d6cc34c1a31834046a4764ac3c3b
Killsan <reversflash47@gmail.com>
2020-05-08 21:26:04 +0300

    python

--- django_site/polls/views.py
+++ django_site/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+# FUCK THIS FUCKING FRAMEWORK
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello world with django")
+	return HttpResponse("Fuck django")
